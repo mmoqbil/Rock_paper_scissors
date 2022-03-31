4aeb2e1ea19a86c4e8667a35ee3348e54b547bf0
JedrzejSad <jedrzejsadlon@gmail.com>
2022-03-31 10:29:14 +0200

    last version

--- papier_nozyce.py
+++ papier_nozyce.py
@@ -6,40 +6,40 @@ def check_who_won(player,computer):
     global computer_wins
     global your_wins
     if player == computer:
-        print("Padł remis!")
+        print("It's draw!")
     elif (player == "Rock") and (computer == "Paper"):
-        print("Komputer wygrał!")
+        print("You lose!")
         computer_wins += 1
     elif (player == "Rock") and (computer == "Scissors"):
-        print("Brawo! Wygrałeś!")
+        print("You win!")
         your_wins += 1
     elif (player=="Paper") and (computer == "Scissors"):
-        print("Komputer wygrał!")
+        print("You Lose!")
         computer_wins += 1
     elif (player == "Paper") and (computer == "Rock"):
-        print("Brawo! Wygrałeś!")
+        print("You win!")
         your_wins += 1
     elif (player=="Scissors") and (computer == "Rock"):
-        print("Komputer wygrał!")
+        print("You lose!")
         computer_wins+=1
     elif (player=="Scissors") and (computer == "Paper"):
-        print("Brawo! Wygrałeś!")
+        print("You win!")
         your_wins += 1
         
 while (play_again == "Y"):
     choices = ["Rock","Paper","Scissors"]
     computer_choice = random.choice(choices)
-    print("Witamy w grze 'Rock, Paper or Scissors")
-    your_choice=input("Wpisz Rock, Paper albo Scissors(wielkość liter ma znaczenie): ")
+    print("Wellcome into 'Rock, Paper or Scissors'")
+    your_choice=input("Type: Rock, Paper or Scissors(Letter size matter): ")
     while (your_choice!="Rock") and (your_choice!="Paper") and (your_choice!="Scissors"):
-        print("Wprowadziłeś zły kod")
-        your_choice=input("Wpisz ponownie: ")
-    print("Ty:",your_choice)
-    print("Komputer:",computer_choice)
+        print("Wrong word")
+        your_choice=input("Type again: ")
+    print("Your choice:",your_choice)
+    print("Computer:",computer_choice)
     check_who_won(your_choice,computer_choice)
-    print("Wygrales",your_wins,"razy")
-    print("Komputer wygral",computer_wins,"razy")
-    play_again = input("Chcesz zagrać ponownie? Y-Tak, N-Nie: ")
+    print("You won: ",your_wins,"times")
+    print("Computer won: ",computer_wins,"times")
+    play_again = input("Wanna play again? Y/N ")
     while (play_again != "Y" and play_again != "N"):
-        play_again = input("Wprowadź Y gdy chcesz grać dalej i N gdy chcesz zakończyć!: ")
+        play_again = input("Type Y to continue and N to quit!: ")
     
